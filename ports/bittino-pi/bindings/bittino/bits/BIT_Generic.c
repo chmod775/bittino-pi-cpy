17bd63a485dded644be9241e034c382e4990f8cb
chmod775 <trombettamichele@yahoo.com>
2026-01-31 17:46:20 +0100

    - Added write method to BIT_Generic class

--- ports/bittino-pi/bindings/bittino/bits/BIT_Generic.c
+++ ports/bittino-pi/bindings/bittino/bits/BIT_Generic.c
@@ -21,7 +21,7 @@ static mp_obj_t bittino_BIT_Generic_make_new(const mp_obj_type_t *type, size_t n
 
     self->realtimes.count_relatime_in = in_count;
     self->realtimes.count_relatime_out = out_count;
-    
+
     self->realtimes.relatime_in = port_malloc(in_count, false);
     self->realtimes.relatime_out = port_malloc(out_count, false);
 
@@ -40,6 +40,26 @@ static mp_obj_t bittino_BIT_Generic_deinit(mp_obj_t self_in) {
 }
 static MP_DEFINE_CONST_FUN_OBJ_1(bittino_BIT_Generic_deinit_obj, bittino_BIT_Generic_deinit);
 
+static mp_obj_t bittino_BIT_Generic_write(mp_obj_t self_in, mp_obj_t seq_in) {
+    bittino_bit_generic_obj_t *self = (bittino_bit_generic_obj_t *)self_in;
+    if (!mp_obj_is_type(seq_in, &mp_type_list) && !mp_obj_is_type(seq_in, &mp_type_tuple)) {
+        mp_raise_TypeError(MP_ERROR_TEXT("expected list/tuple"));
+    }
+
+    size_t n;
+    mp_obj_t *items;
+    mp_obj_get_array(seq_in, &n, &items);
+
+    for (size_t i = 0; i < n; i++) {
+        uint8_t value = mp_obj_get_int(items[i]);
+        self->realtimes.relatime_in[i] = value;
+    }
+
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_2(bittino_BIT_Generic_write_obj, bittino_BIT_Generic_write);
+
+
 static bittino_realtimes_t* bittino_BIT_Generic_get_realtimes_proto(mp_obj_t self_in) {
     bittino_bit_generic_obj_t *self = (bittino_bit_generic_obj_t *)self_in;
     return &self->realtimes;
@@ -53,6 +73,7 @@ static const bittino_module_p_t bittino_BIT_Generic_proto = {
 
 static const mp_rom_map_elem_t bittino_BIT_Generic_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&bittino_BIT_Generic_deinit_obj) },
+    { MP_ROM_QSTR(MP_QSTR_write), MP_ROM_PTR(&bittino_BIT_Generic_write_obj) },
 };
 static MP_DEFINE_CONST_DICT(bittino_BIT_Generic_locals_dict, bittino_BIT_Generic_locals_dict_table);
 
